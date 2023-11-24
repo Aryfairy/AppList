4fde297e6c09474196057ebc2d50ec81355f5158
Aryfairy <ryllarygfs@gmail.com>
2023-11-23 22:12:44 -0300

    clases

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,12 +1,14 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView idRecyclerViewFilmes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
 
         idRecyclerViewFilmes = findViewById(R.id.idRecyclerViewFilmes);
 
+        idRecyclerViewFilmes.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+
+        idRecyclerViewFilmes.setHasFixedSize(true);
+
+        //idRecyclerViewFilmes.setAdapter(adapter);
 
     }
 }
